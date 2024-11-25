8fb5763a6a440f1621d1e37e326b42ba8f571682
Ildar Khuzin <ildar.khuzin75@mail.ru>
2024-11-25 22:25:13 +0300

    26 урок. server+client+gateway+config через github

--- client2/src/main/java/ru/javabegin/client2/controller/TestController.java
+++ client2/src/main/java/ru/javabegin/client2/controller/TestController.java
@@ -1,5 +1,6 @@
 package ru.javabegin.client2.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/new")
 public class TestController {
 
+    @Value("${tmpVar}")
+    private int tmpVar;
+
     @GetMapping("/name")
     public String name(){
-        return "Hello World";
+
+        return "временная переменная " + tmpVar;
     }
 }
